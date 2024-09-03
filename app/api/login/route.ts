d9be048fabee19e71b9ce1939315632662694733
Nasvabyss <Nasifafathima15@gmail.com>
2024-09-03 18:25:39 +0800

    Login function works

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,44 +1,43 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import bcrypt from 'bcryptjs';
 import { db as prisma } from '@/lib/db';
 import { sign } from 'jsonwebtoken';
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-    try {
-      const { email, password } = req.body;
-  
-      console.log('Login request received');
-      console.log('Received data:', { email, password });
-  
-      if (!email || !password) {
-        return res.status(400).json({ message: 'Email and Password are required' });
-      }
-  
-      // Find the user by email
-      const user = await prisma.user.findUnique({ where: { email } });
-  
-      if (!user) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-      }
-  
-      // Check if the password is correct
-      const isValid = await bcrypt.compare(password, user.password);
-  
-      if (!isValid) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-      }
-  
-      // Create JWT token
-      const token = sign(
-        { userId: user.id, email: user.email },
-        process.env.JWT_SECRET as string,
-        { expiresIn: '1h' }
-      );
-  
-      return res.status(200).json({ message: 'Login successful', token });
-    } catch (error) {
-      console.error('Login error:', error);
-      return res.status(500).json({ message: 'Internal server error' });
+export async function POST(req: NextRequest) {
+  try {
+    const { email, password } = await req.json();
+
+    console.log('Login request received');
+    console.log('Received data:', { email, password });
+
+    if (!email || !password) {
+      return NextResponse.json({ message: 'Email and Password are required' }, { status: 400 });
     }
+
+    // Find the user by email
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      return NextResponse.json({ message: 'Invalid email or password' }, { status: 401 });
+    }
+
+    // Check if the password is correct
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+      return NextResponse.json({ message: 'Invalid email or password' }, { status: 401 });
+    }
+
+    // Create JWT token
+    const token = sign(
+      { userId: user.id, email: user.email },
+      process.env.JWT_SECRET as string, // Use a secret from your environment variables
+      { expiresIn: '1h' } // Token expires in 1 hour
+    );
+
+    return NextResponse.json({ message: 'Login successful', token }, { status: 200 });
+  } catch (error) {
+    console.error('Login error:', error);
+    return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
+  }
 }
-  
